f9bfb716dc5d5680da0903e47a5857140bc8451f
SRJeanAguiar <133389307+SRJeanAguiar@users.noreply.github.com>
2023-12-04 21:30:21 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'id21621945_zardoky';
+$dbpasswd = 'Amarelo@@102030';
+$db_name = 'id21621945_projetofaculdade';
+
+$conexao = new mysqli($dbhost, $dbuser, $dbpasswd, $db_name);
+
+?>
